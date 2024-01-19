02291f366e2cfd6b48f84a0d78ad41d6d96751dc
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-01-19 12:17:01 +0900

    update

---
+++ solve/1191.py
@@ -0,0 +1,45 @@
+class node:
+    def __init__(self, data, left_node, right_node) -> None:
+        self.data = data
+        self.left_node = left_node
+        self.right_node = right_node
+        
+def pre_(node):
+    print(node.data, end='')
+    if node.left_node != "." and node.left_node !="None":
+        pre_(tree[node.left_node])
+    if node.right_node != "." and node.right_node !="None":
+        pre_(tree[node.right_node])
+
+def in_(node):
+    
+    if node.left_node != "." and node.left_node !="None":
+        in_(tree[node.left_node])
+    print(node.data, end='')    
+    if node.right_node != "." and node.right_node !="None":
+        in_(tree[node.right_node])
+        
+def post_(node):
+    
+    if node.left_node != "." and node.left_node !="None" :
+        post_(tree[node.left_node])
+    if node.right_node != "." and node.right_node !="None":
+        post_(tree[node.right_node])
+    print(node.data, end='')
+
+
+n = int(input())
+tree = {}
+for i in range(n):
+    data,left_node,right_node = input().split()
+    tree[data] = node(data,left_node,right_node)
+    
+    
+    
+
+pre_(tree['A'])
+print()
+in_(tree['A'])
+print()
+post_(tree['A'])
+print()
