81b5000158409337fee141746ae64eb2f2ae81ea
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-01-24 00:27:36 +0900

    [Gold II] Title: 장난감 조립, Time: 60 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+
+lists = [[0 for _ in range(N+1)] for _ in range(N+1)]
+destination = [0 for _ in range(N+1)]
+destination[0] = 1
+container = [False for _ in range(N+1)]
+for i in range(M):
+    a, b, c = map(int, sys.stdin.readline().split())
+    destination[b] += 1
+    lists[a][b] = c
+    container[a] = True
+
+seq = deque()
+visited = [False for _ in range(N+1)]
+while destination.count(0) < N:
+    for i in range(1, N+1):
+        if destination[i] == 0 and not visited[i]and container[i]:
+            seq.append(i)
+            visited[i] = True
+            for j in range(1, N+1):
+                if lists[i][j] != 0 :
+                    destination[j] -= 1
+seq.popleft()
+
+
+while True:
+    a = seq.popleft()
+    for i in range(1,N+1):
+        lists[N][i] += lists[a][i]*lists[N][a]
+        
+    lists[N][a] = 0   
+        
+    if len(seq)  == 0:
+        break   
+
+
+for i in range(1,N+1):
+    if not container[i]:
+        print(i ,lists[N][i])
+    
+  
