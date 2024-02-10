a2d6415e6758c10a9557c26d7581e70b72ce5183
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-02-10 13:29:27 +0900

    [Gold I] Title: K번째 수, Time: 1104 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,27 @@
+N = int(input())
+K = int(input())
+
+start = 1
+end = N*N
+ans = 0
+
+while start <= end:
+    count = 0
+    mid = (start + end)//2
+    
+    for i in range(1,1+N):
+        count += min(mid//i , N)
+        
+        
+    if count >= K:
+        ans = mid  
+        end = mid-1
+    else:
+        start = mid+1
+        
+print(ans)
+
+
+
+
+       
