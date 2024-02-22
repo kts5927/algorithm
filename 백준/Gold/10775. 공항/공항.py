f815799aa56ce64c7e87f55dab4cdf6a4c54cd80
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-02-22 10:32:15 +0900

    [Gold II] Title: 공항, Time: 124 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.py
@@ -0,0 +1,34 @@
+import sys
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+def main():
+    G = int(sys.stdin.readline().strip())
+    P = int(sys.stdin.readline().strip())
+
+    parent = [i for i in range(G + 1)]
+
+    count = 0
+    for _ in range(P):
+        gi = int(sys.stdin.readline().strip())
+        root = find_parent(parent, gi)
+        if root == 0:
+            break
+        union_parent(parent, root, root - 1)
+        count += 1
+
+    print(count)
+
+if __name__ == "__main__":
+    main()
