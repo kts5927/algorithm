3d4c956245d8243a43b83f6f053291093de8b4ae
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-02-25 15:53:46 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 2584 ms, Memory: 61636 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
@@ -1,53 +1,53 @@
+import sys
 from collections import deque
-input = __import__('sys').stdin.readline
 
-def bfs(start):
-    q = deque()
-    q.append(start)
-    cnt = 1
-    while q:
-        i, j = q.popleft()
-        zeros[i][j] = group
-        for idx in range(4):
-            ni, nj = i + dy[idx], j + dx[idx]
-            if ni < 0 or ni >= n or nj < 0 or nj >= m or visited[ni][nj] or graph[ni][nj] == 1:
-                continue
-            visited[ni][nj] = True
-            q.append((ni, nj))
-            cnt += 1
-    return cnt
+N, M = map(int, sys.stdin.readline().split())
+table = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().strip())) for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
-zeros = [[0] * m for _ in range(n)]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+shadow = [[0 for _ in range(M)] for _ in range(N)]
+visited = [[False] * M for _ in range(N)]
+place = {}
+place[0] = 0
+
+def BFS(i, j):
+    que = deque()
+    que.append([i,j])
+    count = 1
+    while que:
+        a, b = que.popleft()
+        shadow[a][b] = group
+        for k in range(4):
+            x = dx[k] + a
+            y = dy[k] + b
+            if 0 <= x < N and 0 <= y < M and not visited[x][y] and table[x][y] == 0:
+                que.append((x, y))
+                visited[x][y] = True
+                count += 1
+    return count
+
 group = 1
-info = {}
-info[0] = 0
-
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 0 and not visited[i][j]:
+for i in range(N):
+    for j in range(M):
+        if table[i][j] == 0 and not visited[i][j]:
             visited[i][j] = True
-            w = bfs((i, j))
-            info[group] = w
+            cnt = BFS(i, j)
+            place[group] = cnt
             group += 1
 
-for i in range(n):
-    for j in range(m):
-        addList = set()
-        if graph[i][j]:
-            for idx in range(4):
-                ni, nj = i + dy[idx], j + dx[idx]
-                if ni < 0 or ni >= n or nj < 0 or nj >= m:
-                    continue
-                addList.add(zeros[ni][nj])
-            for add in addList:
-                graph[i][j] += info[add]
-                graph[i][j] %= 10
+for i in range(N):
+    for j in range(M):
+        if table[i][j] == 1:
+            neighbors = set()
+            for k in range(4):
+                x = dx[k] + i
+                y = dy[k] + j
+                if 0 <= x < N and 0 <= y < M:
+                    neighbors.add(shadow[x][y])
+            for z in neighbors:
+                table[i][j] += place[z]
+            table[i][j] %= 10
 
-for g in graph:
-    print("".join(map(str, g)))
+for row in table:
+    print(*row, sep='')
