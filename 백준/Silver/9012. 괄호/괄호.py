013d401887c891f8e447525794fe87d382da1307
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-02-29 19:21:19 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,20 @@
+
+import sys
+N = int(input())
+for i in range(N):
+    arr = list(map(str,input().strip()))
+    ans = []
+    br = True
+    for j in arr:
+        if j == '(':
+            ans.append(j)
+        elif j == ')':
+            if len(ans) == 0:
+                print('NO')
+                br = False
+                break
+            else : ans.pop()
+    if br:
+        if len(ans) == 0:
+            print('YES')
+        else : print('NO')
