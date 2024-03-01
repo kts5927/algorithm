503562cd1ee7b58755a2b880ca4846dd4c3de244
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-03-02 00:34:54 +0900

    [Silver II] Title: 폴짝폴짝, Time: 760 ms, Memory: 114272 KB -BaekjoonHub

---
+++ 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+N = int(sys.stdin.readline())
+bridge = list(map(int,sys.stdin.readline().split()))
+start , end = map(int,sys.stdin.readline().split())
+visit = [False  for _ in range(N+1)]
+visit[start - 1] =True
+
+jump = deque()
+jump.append([start - 1 , 0])
+
+while jump:
+    a , b = jump.popleft()
+    
+    if a == end-1:
+        print(b)
+        sys.exit()
+    for i in range(N):
+        if not visit[i] and abs(i-a) % bridge[a] == 0:
+            jump.append([i , b+1])
+            visit[i] = True
+            
+print(-1)
