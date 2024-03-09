bb3e9735b7a89c8447567f82966873992497796d
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-03-09 16:01:09 +0900

    [Platinum III] Title: 아이돌, Time: 492 ms, Memory: 31260 KB -BaekjoonHub

--- 백준/Platinum/3648. 아이돌/아이돌.py
+++ 백준/Platinum/3648. 아이돌/아이돌.py
@@ -1,59 +1,54 @@
 import sys
 sys.setrecursionlimit(10 ** 5)
-input = sys.stdin.readline
 
 def SCC(node):
-    global idx, scc_num
+    global idx , scc_num
     visit[node] = idx
+    root = idx
     idx += 1
     stack.append(node)
-    root = visit[node]
-    for nxt in graph[node]:
-        if not visit[nxt]:
-            root = min(root, SCC(nxt))
-        elif not check[nxt]:
-            root = min(root, visit[nxt])
+    for next in arr[node]:
+        if not visit[next]:
+            root = min(root,SCC(next))
+        elif not check[next]:
+            root = min(root,visit[next])
     if root == visit[node]:
-
-        while True:
+        while stack:
             top = stack.pop()
-            check[top] = True
-            CNF[top] = scc_num
-            if top == node:
+            check[top] = 1
+            scc_idx[top] = scc_num
+            if node == top:
                 break
-        scc_num+=1
+        scc_num += 1
     return root
 
 while True:
-    inputline  = input()
-    if inputline == "":
+    
+    try:
+        N , M = map(int,input().split())
+    except EOFError:
         break
-    N, M = map(int, inputline.split())
-    graph = [[] for _ in range(2*N)]
+    arr = [[] for _ in range(2*N+1)]
+
     for _ in range(M):
-        a, b = map(int, input().split())
-        if a < 0 :  a = N - a
-        if b < 0 : b = N -b
-        a -= 1
-        b -= 1
-        graph[(a+N)%(2*N)].append(b)
-        graph[(b+N)%(2*N)].append(a)
-    graph[N].append(0)
-
-    visit = [False] * (2*N)
-    check = [False] * (2*N)
+        a , b = map(int,sys.stdin.readline().split())
+        arr[-a].append(b)
+        arr[-b].append(a)
+    arr[-1].append(1)
+    scc_num = 1    
     idx = 1
-    scc_num = 0
-    CNF = [-1]*(2*N)
     stack = []
-    scc_arr = []
-
-    for i in range(2*N):
+    scc_idx = [0]*(2*N+1)
+    visit = [0]*(2*N+1)
+    check = [0]*(2*N+1)
+        
+    for i in range(1,N+1):
         if not visit[i]:
             SCC(i)
-    for i in range(N):
-        if CNF[i] == CNF[N+i]:
-            print("no")
+            
+    for i in range(1,N+1):
+        if scc_idx[i] == scc_idx[-i]:
+            print('no')
             break
     else:
-        print("yes")
+        print('yes')
