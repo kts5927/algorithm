93e7391620d9f56b158633b5f29c688df354be58
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-03-14 11:18:03 +0900

    [Silver II] Title: 스택 수열, Time: 260 ms, Memory: 32704 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,24 @@
+import sys
+N = int(sys.stdin.readline())
+stack = []
+cal = [0]
+ans = []
+tf = True
+num = 1
+for i in range(N):
+    stack.append(int(sys.stdin.readline()))
+    while True:
+        if stack[0] > cal[-1]:
+            ans.append('+')
+            cal.append(num)
+            num += 1
+        elif stack[0] == cal[-1]:
+            cal.pop()
+            stack.pop()
+            ans.append('-')
+            break
+        else : 
+            print('NO')
+            exit()
+for i in ans:
+    print(*i , end = '\n')
