e6d03908f902c5096156381bd30a108a70d093b8
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-03-22 10:52:56 +0900

    [level 1] Title: 폰켓몬, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,19 +1,3 @@
 def solution(nums):
-    
-    answer = 0
-    
-    n = len(nums)/2
-    nums.sort()
-    count = 0
-    number = 0
-    for i in nums:
-        if number != i:
-            count += 1
-            number = i
-    
-    if count >= n:
-        answer = n
-    else : 
-        answer = count
-    
-    return answer
+
+    return min(len(nums)/2 , len(set(nums)))
