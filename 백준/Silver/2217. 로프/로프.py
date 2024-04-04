dfd45c1ffdf4cb126d3c73ffc98019686ab5bfec
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-04-04 21:19:24 +0900

    [Silver IV] Title: 로프, Time: 4032 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,22 @@
+import heapq
+
+strings = []
+
+N = int(input())
+
+for i in range(N):
+    heapq.heappush(strings,-int(input()))
+    
+weight = 0
+count = 1
+ans = 0
+for i in range(N):
+    
+    rope = -heapq.heappop(strings)
+    
+    if (count * rope) > weight:
+        weight = count * rope
+        ans = rope * count
+    count += 1
+    
+print(ans)
