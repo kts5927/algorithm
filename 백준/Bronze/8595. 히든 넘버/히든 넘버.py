511c995bd3fc657a22eeb0eadb9d9b17e5152c68
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-04-05 14:09:01 +0900

    [Bronze I] Title: 히든 넘버, Time: 1372 ms, Memory: 87568 KB -BaekjoonHub

---
+++ 백준/Bronze/8595. 히든 넘버/히든 넘버.py
@@ -0,0 +1,19 @@
+import sys
+N = int(input())
+lst = list(map(str,input().strip()))
+count = ''
+ans = 0
+
+for i in lst:
+    if '0'<= i <= '9':
+        count+=i
+    else:
+        if count == '':
+            continue
+        else:
+            ans += int(count)
+            count = ''
+
+if count != '':
+    ans += int(count)
+print(ans)
