f79f9e9fc0d31996de2f9b3c4c23179d3293cbfa
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-05-17 10:12:23 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -2,8 +2,9 @@ N = int(input())
 lst = list(map(int,input().split()))
 lst.sort()
 ans = 0
-for i in range(1,len(lst)+1):
-    for j in lst[:i]:
-       ans += j
+cal = 0
+for i in lst:
+    cal += i
+    ans += cal
        
 print(ans) 
