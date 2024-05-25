7032c1f77e8c23b4bb54a6a0e7e052e5acfd2eb0
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-05-25 09:51:08 +0900

    [Silver II] Title: 침투, Time: 1068 ms, Memory: 38236 KB -BaekjoonHub

---
+++ 백준/Silver/13565. 침투/침투.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+N , M = map(int,input().split())
+lst = [list(map(int,input().strip())) for _ in range(N)]
+
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+ans = False
+for i in range(M):
+    if lst[0][i] == 0:
+        cal = deque()
+        cal.append([0,i])
+        lst[0][i] = 1
+        while cal:
+            raw, colum = cal.popleft()
+            
+            for j in range(4):
+                x = colum + dx[j]
+                y = raw + dy[j]
+                if 0<=x<M and 0<=y<N and lst[y][x] == 0:
+                    if y == N-1:
+                        ans = True
+                        break
+                    cal.append([y,x])
+                    lst[y][x] = 1
+
+if ans:
+    print('YES')
+else:
+    print('NO')
