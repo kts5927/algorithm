be5d2bb7e46ff8f85bcee902ec298dea6133ec3d
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-06-05 14:35:31 +0900

    [Bronze II] Title: 암호, Time: 44 ms, Memory: 31620 KB -BaekjoonHub

---
+++ 백준/Bronze/1718. 암호/암호.py
@@ -0,0 +1,17 @@
+normal = list(str(input()))
+compare = list(str(input()))
+
+passwordkey = compare*(len(normal) // len(compare) + 1)
+ans = []
+# print(normal)
+for i in range(len(normal)):
+    if normal[i] == ' ':
+        ans.append(' ')
+    else:
+
+        cal = (ord(normal[i]))-ord(passwordkey[i])+96
+        if cal < 97:
+            cal += 26
+        ans.append(chr(cal))
+print(''.join(ans))
+        
