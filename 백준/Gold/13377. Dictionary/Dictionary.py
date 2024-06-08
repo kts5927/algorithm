a993152494475557cf8361c76b6af22021b2bcf1
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-06-08 15:09:17 +0900

    [Gold V] Title: Dictionary, Time: 312 ms, Memory: 97468 KB -BaekjoonHub

--- 백준/Gold/13377. Dictionary/Dictionary.py
+++ 백준/Gold/13377. Dictionary/Dictionary.py
@@ -5,8 +5,14 @@ alphabet = ['a','b','c','d','e','f','g','h','i']
 lst = list(permutations(alphabet,9))
 for i in range(362880):
     lst[i] = ''.join(lst[i])
+
+check = {}
+i = 1
+for w in lst:
+    check[w] = i
+    i += 1
     
 N = int(sys.stdin.readline())
 for i in range(N):
     string = sys.stdin.readline().strip()
-    print(lst.index(string)+1)
+    print(check[string])
