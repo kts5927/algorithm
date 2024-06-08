eefb80ae315bfdfbeb3bd794b10e43a92c818ca3
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-06-08 15:06:07 +0900

    [Gold V] Title: Dictionary, Time: 5484 ms, Memory: 182564 KB -BaekjoonHub

--- 백준/Gold/13377. Dictionary/Dictionary.py
+++ 백준/Gold/13377. Dictionary/Dictionary.py
@@ -1,11 +1,12 @@
 from itertools import permutations
+import sys
 
 alphabet = ['a','b','c','d','e','f','g','h','i']
 lst = list(permutations(alphabet,9))
 for i in range(362880):
     lst[i] = ''.join(lst[i])
     
-N = int(input())
+N = int(sys.stdin.readline())
 for i in range(N):
-    string = input()
+    string = sys.stdin.readline().strip()
     print(lst.index(string)+1)
