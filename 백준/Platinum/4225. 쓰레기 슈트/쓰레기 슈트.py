91d6327548da720f2ecfbbff5d24e14a6af06af1
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-06-15 15:56:00 +0900

    [Platinum III] Title: 쓰레기 슈트, Time: 104 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Platinum/4225. 쓰레기 슈트/쓰레기 슈트.py
@@ -0,0 +1,58 @@
+import sys
+import math
+input = sys.stdin.readline
+MAX = float('inf')
+
+def ccw(a, b, c):
+    return (a[0]*b[1] + b[0]*c[1] + c[0]*a[1]) - (a[1]*b[0] + b[1]*c[0] + c[1]*a[0])
+
+def polar_angle(p0, p1):
+    return math.atan2(p1[1] - p0[1], p1[0] - p0[0])
+
+def distance_squared(p0, p1):
+    return (p1[0] - p0[0]) ** 2 + (p1[1] - p0[1]) ** 2
+
+def convex_hull(points):
+    stk = points[:2]
+    for point in points[2:]:
+        while len(stk) > 1 and ccw(stk[-2], stk[-1], point) <= 0:
+            stk.pop()
+        stk.append(point)
+    return stk
+
+def distance(target, p0, p1):
+    if p0[0] == p1[0]:
+        return abs(target[0] - p0[0])
+    if p0[1] == p1[1]:
+        return abs(target[1] - p0[1])
+    a, b = (p1[1] - p0[1]) / (p1[0] - p0[0]), -1
+    c = -a * p0[0] + p0[1]
+    return abs(a*target[0] + b*target[1] + c) / (a**2 + b**2) ** 0.5
+
+def solve(idx):
+    N = int(input())
+    if N == 0:
+        return False
+    points = [list(map(int, input().split())) for _ in range(N)]
+    ref, *points = sorted(points)
+    points.sort(key=lambda p: (polar_angle(ref, p), distance_squared(ref, p)))
+
+    result = convex_hull([ref] + points)
+    length = len(result)
+
+    ans = MAX
+    for i in range(length):
+        k = (i + 1) % length
+        tmp = 0.
+        for j in range(length):
+            tmp = max(tmp, distance(result[j], result[i], result[k]))
+        ans = min(ans, tmp)
+
+    print(f'Case {idx}: {math.ceil(ans * 100) / 100.0:.2f}')
+    return True
+
+idx = 1
+while True:
+    if not solve(idx):
+        break
+    idx += 1
