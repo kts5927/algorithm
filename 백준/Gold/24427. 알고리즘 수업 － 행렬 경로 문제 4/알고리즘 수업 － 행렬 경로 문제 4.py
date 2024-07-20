1567834cbaa0bdaaa75e2c56100c2bb72c330949
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-07-20 17:54:26 +0900

    [Gold IV] Title: 알고리즘 수업 - 행렬 경로 문제 4, Time: 2036 ms, Memory: 290288 KB -BaekjoonHub

---
+++ 백준/Gold/24427. 알고리즘 수업 － 행렬 경로 문제 4/알고리즘 수업 － 행렬 경로 문제 4.py
@@ -0,0 +1,27 @@
+import sys
+
+def path():
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + lst[i-1][j-1]
+
+N = int(sys.stdin.readline().strip())
+lst = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
+dp = [[0] * (N+1) for _ in range(N+1)]
+
+P = int(sys.stdin.readline().strip())
+points = {tuple(map(int, sys.stdin.readline().strip().split())) for _ in range(P)}
+
+path()
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if (i, j) not in points:
+            dp[i][j] = 0
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if dp[i-1][j] != 0 or dp[i][j-1] != 0:
+            dp[i][j] = max(dp[i][j],lst[i-1][j-1] + max(dp[i-1][j], dp[i][j-1]) )
+
+print(dp[N][N])
