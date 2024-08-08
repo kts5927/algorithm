f6421854af42231b81e5fa80cd80adadff5dc7ff
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-08-08 09:23:38 +0900

    update

--- solve/1423.py
+++ solve/1423.py
@@ -6,13 +6,14 @@ level = list(map(int,sys.stdin.readline().split()))
 Str = list(map(int,sys.stdin.readline().split()))
 Day = int(sys.stdin.readline())
 
-cal = [[0]*len(level) for _ in range(min(len(level),Day))]
+cal = [[0]*len(level) for _ in range(len(level)-1)]
 
 for i in range(len(cal)):
     for j in range(len(cal[0])):
         if j > i:
             cal[i][j-i] = (Str[j]-Str[i]) / (j-i)
-
+for i in cal:
+    print(*i)
 heap = []
 for i in range(len(cal)):
     for j in range(len(cal[0])):
@@ -21,7 +22,7 @@ for i in range(len(cal)):
 
 while heap:
     value, s, e = heapq.heappop(heap)
-    
+    print(s,e)
     if level[s] != 0:
         mx = min(Day//e , level[s])
         level[s] -= mx
