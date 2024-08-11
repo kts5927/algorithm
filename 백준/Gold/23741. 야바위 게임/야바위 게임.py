ea1a6ca3ea916d35b1e5f78efe6b2298890a644c
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-08-12 01:35:16 +0900

    [Gold IV] Title: 야바위 게임, Time: 80 ms, Memory: 37988 KB -BaekjoonHub

--- 백준/Gold/23741. 야바위 게임/야바위 게임.py
+++ 백준/Gold/23741. 야바위 게임/야바위 게임.py
@@ -1,54 +1,52 @@
 import sys
 from collections import deque
 
-N,M,X,Y = map(int,sys.stdin.readline().split())
-lst = [[] for _ in range(N+1)]
+N, M, X, Y = map(int, sys.stdin.readline().split())
+lst = [[] for _ in range(N + 1)]
 for i in range(M):
-    a , b = map(int,sys.stdin.readline().split())
+    a, b = map(int, sys.stdin.readline().split())
     lst[a].append(b)
     lst[b].append(a)
-    
-dp = [[0]*(N+1) for _ in range(Y+1)]
+
+dp = [[0] * (N + 1) for _ in range(Y + 1)]
 location = deque()
-visited_even = []
-visited_odd = []
+visited_even = [False] * (N + 1)
+visited_odd = [False] * (N + 1)
+
 for i in lst[X]:
-    location.append([i,1])
-    visited_even.append(i)
-    
+    location.append([i, 1])
+    visited_even[i] = True
+
 while location:
-    e , t = location.popleft()
+    e, t = location.popleft()
     if t > Y:
         break
     dp[t][e] += 1
 
     for i in lst[e]:
-        if t%2 == 1:
-            if i not in visited_odd and t <= Y:
-                location.append([i,t+1])
-                visited_odd.append(i)
+        if t % 2 == 1:
+            if not visited_odd[i] and t <= Y:
+                location.append([i, t + 1])
+                visited_odd[i] = True
         else:
-            if i not in visited_even and t <= Y:
-                location.append([i,t+1])
-                visited_even.append(i)
-               
+            if not visited_even[i] and t <= Y:
+                location.append([i, t + 1])
+                visited_even[i] = True
+
 ans = []
-if Y%2 == 0:
-    if len(visited_odd) == 0:
+if Y % 2 == 0:
+    if not any(visited_odd):
         print(-1)
     else:
-        visited_odd.sort()
-        for i in visited_odd:
-            if i not in ans:
+        for i in range(1, N + 1):
+            if visited_odd[i]:
                 ans.append(i)
         print(*ans)
 else:
-    if len(visited_even) == 0:
+    if not any(visited_even):
         print(-1)
     else:
-        visited_even.sort()
-        for i in visited_even:
-            if i not in ans:
+        for i in range(1, N + 1):
+            if visited_even[i]:
                 ans.append(i)
         print(*ans)
-
