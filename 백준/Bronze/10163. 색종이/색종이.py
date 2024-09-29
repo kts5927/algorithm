5e068718ffe8f29146ecbad23cbb8fb20fbf4706
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-09-29 22:23:44 +0900

    [Bronze I] Title: 색종이, Time: 676 ms, Memory: 116268 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,18 +1,17 @@
 lst = [[0]*1001 for _ in range(1001)]
 
 N = int(input())
+
+ans = [0]*1002
 for k in range(N):
     a,b,c,d = map(int,input().split())
     
     for i in range(c):
         for j in range(d):
+            ans[k+1] += 1
+            ans[lst[i+a][j+b]] -= 1
+            
             lst[i+a][j+b] = k+1
-
-
-ans = [0]*1002
-for i in range(1001):
-    for j in range(1001):
-        ans[lst[i][j]] += 1
-        
+            
 for i in range(N):
     print(ans[i+1])
