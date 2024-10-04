f4ead361cbaba361fdc271a05596f4bbc6358817
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-10-04 22:44:24 +0900

    [level 3] Title: 표 병합, Time: 231.43 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.py
+++ 프로그래머스/3/150366. 표 병합/표 병합.py
@@ -1,24 +1,30 @@
 def solution(commands):
     answer = []
-    # 병합 상태 저장하는 배열 및 스프레드시트 초기화
-    merged = [[(i, j) for j in range(50)] for i in range(50)]
-    board = [["EMPTY"] * 50 for _ in range(50)]
     
-    # 명령어 처리
-    for command in commands:
-        command = command.split(' ')
+    merged = [[(i, j) for j in range(50)] for i in range(50)]
+    Table = [['EMPTY'] * 50 for _ in range(50)]
+    
+    for data in commands:
+        command = data.split()
         
         if command[0] == 'UPDATE':
-            if len(command) == 4:  # 특정 셀 업데이트
-                r, c, value = int(command[1])-1, int(command[2])-1, command[3]
+            if len(command) == 4:
+                r = int(command[1]) - 1
+                c = int(command[2]) - 1
+                Value = command[3]
+                
+                # 병합된 셀 처리
                 x, y = merged[r][c]
-                board[x][y] = value
-            elif len(command) == 3:  # 특정 값 전체 업데이트
-                value1, value2 = command[1], command[2]
+                Table[x][y] = Value
+                
+            elif len(command) == 3:
+                Value1, Value2 = command[1], command[2]
+                
+                # Table 전체 탐색하여 값 변경
                 for i in range(50):
                     for j in range(50):
-                        if board[i][j] == value1:
-                            board[i][j] = value2
+                        if Table[i][j] == Value1:
+                            Table[i][j] = Value2
         
         elif command[0] == 'MERGE':
             r1, c1, r2, c2 = int(command[1])-1, int(command[2])-1, int(command[3])-1, int(command[4])-1
@@ -26,33 +32,33 @@ def solution(commands):
             x2, y2 = merged[r2][c2]
             
             # 병합: 빈 셀인 경우 값 복사
-            if board[x1][y1] == "EMPTY":
-                board[x1][y1] = board[x2][y2]
+            if Table[x1][y1] == "EMPTY":
+                Table[x1][y1] = Table[x2][y2]
             
             # 두 번째 셀이 가리키는 좌표를 첫 번째 셀로 통일
             for i in range(50):
                 for j in range(50):
                     if merged[i][j] == (x2, y2):
                         merged[i][j] = (x1, y1)
-        
+            
         elif command[0] == 'UNMERGE':
             r, c = int(command[1])-1, int(command[2])-1
             x, y = merged[r][c]
-            temp_value = board[x][y]  # 병합된 셀의 값 저장
+            temp_value = Table[x][y]  # 병합된 셀의 값 저장
             
             # 병합된 셀들 모두 분리 및 초기화
             for i in range(50):
                 for j in range(50):
                     if merged[i][j] == (x, y):
                         merged[i][j] = (i, j)
-                        board[i][j] = "EMPTY"
+                        Table[i][j] = "EMPTY"
             
             # 분리된 셀의 값 복원
-            board[r][c] = temp_value
-        
+            Table[r][c] = temp_value
+            
         elif command[0] == 'PRINT':
             r, c = int(command[1])-1, int(command[2])-1
             x, y = merged[r][c]
-            answer.append(board[x][y])
+            answer.append(Table[x][y])
     
     return answer
