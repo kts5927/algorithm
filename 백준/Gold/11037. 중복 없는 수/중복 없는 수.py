363303a6db1be904d91c10b8e0a3259e75b602d9
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-10-20 08:09:39 +0900

    [Gold IV] Title: 중복 없는 수, Time: 2108 ms, Memory: 115340 KB -BaekjoonHub

--- 백준/Gold/11037. 중복 없는 수/중복 없는 수.py
+++ 백준/Gold/11037. 중복 없는 수/중복 없는 수.py
@@ -1,22 +1,26 @@
-from itertools import combinations, permutations
-import bisect
 import sys
 
-numbers = []
-digits = '123456789'
-
-for k in range(1, 10):
-    for combo in combinations(digits, k):
-        for perm in permutations(combo):
-            num = int(''.join(perm))
-            numbers.append(num)
-
-numbers.sort()
+def DFS(num, cal, used_digits):
+    if cal > 987654321:
+        return float('inf')
+    
+    min_val = float('inf')
+    if cal > num:
+        min_val = cal
+    
+    for i in range(1, 10):
+        if i not in used_digits:
+            used_digits.add(i)
+            cal_next = DFS(num, cal * 10 + i, used_digits)
+            min_val = min(min_val, cal_next)
+            used_digits.remove(i)  # 백트래킹
+            
+    return min_val
 
 for line in sys.stdin:
     N = int(line.strip())
-    idx = bisect.bisect_right(numbers, N)
-    if idx < len(numbers):
-        print(numbers[idx])
-    else:
+    result = DFS(N, 0, set())
+    if result == float('inf'):
         print(0)
+    else:
+        print(result)
