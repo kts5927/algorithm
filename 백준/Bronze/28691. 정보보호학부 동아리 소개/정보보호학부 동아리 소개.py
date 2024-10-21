392359f93917d34e2a584f2ede3d744b8bad6c14
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-10-21 22:18:22 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+S = str(input())
+if S == 'M':
+    print('MatKor')
+elif S == 'W':
+    print('WiCys')
+elif S == 'C':
+    print('CyKor')
+elif S == 'A':
+    print('AlKor')
+else:
+    print('$clear')
