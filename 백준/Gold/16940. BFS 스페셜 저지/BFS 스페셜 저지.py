9ecf262a89c955bbbb11841e6d2f0407cf421d90
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-10-30 18:43:06 +0900

    [Gold III] Title: BFS 스페셜 저지, Time: 308 ms, Memory: 147420 KB -BaekjoonHub

--- 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.py
+++ 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.py
@@ -5,48 +5,54 @@ def fail():
     print(0)
     sys.exit()
 
+
 N = int(sys.stdin.readline())
-Node = [[] for _ in range(N + 1)]
-for _ in range(N - 1):
-    a, b = map(int, sys.stdin.readline().split())
+Node = [[] for _ in range(N+1)]
+for i in range(N-1):
+    a , b = map(int,sys.stdin.readline().split())
+    
     Node[a].append(b)
     Node[b].append(a)
-
-lst = list(map(int, sys.stdin.readline().split()))
-numb = [0] * N
-distance = [0] * (N + 1)
-child = [[] for _ in range(N + 1)]
-visited = [0] * (N + 1)
+    
+lst = list(map(int,sys.stdin.readline().split()))
+numb = [0 for _ in range(N)]
+distance = [0 for _ in range(N+1)]
+child = [[] for _ in range(N+1)]
+visited = [0 for _ in range(N+1)]
 visited[1] = 1
-
 que = deque()
-que.append((lst[0], 0))
+que.append([lst[0],0])
+
 while que:
     location, dist = que.popleft()
     numb[dist] += 1
     distance[location] = dist
+        
     for i in Node[location]:
         if visited[i] == 0:
-            que.append((i, dist + 1))
+            que.append([i,dist+1])
             child[location].append(i)
             visited[i] = 1
-
-visited = [0] * (N + 1)
+    
+visited = [0 for _ in range(N+1)]
 visited[1] = 1
-pointer = 1
+check = [0 for _ in range(N)]
 
+pointer = 1
 for i in lst:
+    
     if numb[distance[i]] > 0:
         numb[distance[i]] -= 1
     else:
         fail()
-
-    current_child_list = sorted(lst[pointer:pointer + len(child[i])])
-    expected_child_list = sorted(child[i])
-
-    if current_child_list != expected_child_list:
-        fail()
     
+    a = lst[pointer:pointer + len(child[i])]
+    a.sort()
+    child[i].sort()
+    
+    if a != child[i]:
+        fail()
     pointer += len(child[i])
-
+    
+    
 print(1)
