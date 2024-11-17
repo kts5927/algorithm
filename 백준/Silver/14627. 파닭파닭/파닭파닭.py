b09bffc1a4a81ea36b2e3c6df0547a7aa846435f
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-11-17 16:43:59 +0900

    [Silver II] Title: 파닭파닭, Time: 1576 ms, Memory: 71112 KB -BaekjoonHub

---
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+S , C = map(int,input().split())
+onion = []
+for i in range(S):
+    onion.append(int(input()))
+
+L = 1
+R = max(onion)
+ans = 0
+while L <= R:
+    
+    mid = (L + R) // 2
+
+    
+    use = 0
+    use = sum(o // mid for o in onion)
+
+    
+    if use < C:
+        R = mid-1
+    else:
+        ans = max(ans,mid)
+        L = mid+1
+
+print(sum(onion) - (ans*C))     
