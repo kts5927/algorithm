be91e3a88c9bdcab3007251f78a37f317ff82727
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-12-18 22:27:02 +0900

    [Bronze II] Title: 개미, Time: 208 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11880. 개미/개미.py
+++ 백준/Bronze/11880. 개미/개미.py
@@ -1,8 +1,7 @@
+import sys
+input = sys.stdin.readline
 T = int(input())
 for i in range(T):
-    a,b,c = map(int,input().split())
-    A = a**2 + (b+c)**2
-    B = b**2 + (a+c)**2
-    C = c**2 + (a+b)**2
-    
-    print(min(A,B,C))
+    load = list(map(int,input().split()))
+    load.sort()
+    print((load[0]+load[1])**2 + load[2]**2)
