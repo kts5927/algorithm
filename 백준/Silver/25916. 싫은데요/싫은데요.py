2501fd10774f19791b211527a01591aed3b0c9a1
kts5927 <154499036+kts5927@users.noreply.github.com>
2025-01-03 23:29:35 +0900

    [Silver I] Title: 싫은데요, Time: 424 ms, Memory: 89824 KB -BaekjoonHub

--- 백준/Silver/25916. 싫은데요/싫은데요.py
+++ 백준/Silver/25916. 싫은데요/싫은데요.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 N , M = map(int,input().split())
-A = list(map(int,input().split()))
+A = tuple(map(int,input().split()))
 
 Start,End,Sum,ans = 0,0,0,0
 while End < len(A):
@@ -11,6 +11,7 @@ while End < len(A):
     while Sum > M and Start <= End:
         Sum -= A[Start]
         Start += 1
-    ans = max(ans,Sum)
+    if Sum > ans:
+        ans = Sum
     End += 1    
 print(ans)
