5282743c4a5e7d9db61fb7516723d341b05e8dea
kts5927 <154499036+kts5927@users.noreply.github.com>
2025-01-15 00:27:46 +0900

    [Bronze I] Title: 샷건, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32371. 샷건/샷건.py
@@ -0,0 +1,21 @@
+String = []
+for i in range(4):
+    
+    string = list(map(str,input().strip()))
+    for i in range(len(string)):
+        string[i] = ord(string[i])
+    String.append(string)
+    
+string = list(map(str,input().strip()))
+for i in range(len(string)):
+    string[i] = ord(string[i])
+
+cal = []
+for i in range(len(string)):
+    for j in range(4):
+        for k in range(len(String[j])):
+            if string[i] == String[j][k]:
+                cal.append([j,k])
+cal.sort()
+
+print(chr(String[cal[4][0]][cal[4][1]]))
