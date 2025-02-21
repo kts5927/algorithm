c577d542a37e4c058fa63d925992ae923f21ea1f
kts5927 <154499036+kts5927@users.noreply.github.com>
2025-02-22 04:27:45 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,13 @@
+Class = {'Algorithm':204,
+        'DataAnalysis':207,
+        'ArtificialIntelligence':302,
+        'CyberSecurity':'B101',
+        'Network':303,
+        'Startup':501,
+        'TestStrategy':105
+}
+
+N = int(input())
+for i in range(N):
+    where = input()
+    print(Class[where])
