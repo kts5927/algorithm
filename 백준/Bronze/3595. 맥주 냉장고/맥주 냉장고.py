6307906c261b48b1063683c5413fbd49396cf0d1
kts5927 <154499036+kts5927@users.noreply.github.com>
2025-02-28 20:10:40 +0900

    [Bronze II] Title: 맥주 냉장고, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3595. 맥주 냉장고/맥주 냉장고.py
+++ 백준/Bronze/3595. 맥주 냉장고/맥주 냉장고.py
@@ -4,11 +4,11 @@ n = int(input())
 
 ans = [1,1,1]
 Min = float('inf')
-for a in range(1,n+1):
+for a in range(1,int(n**0.5+1)):
     if n%a == 0:
         bc = n//a
         
-        for b in range(1,bc):
+        for b in range(1,int(bc**0.5+1)):
             if bc%b == 0:
                 c = bc//b
                 if a*b + b*c + c*a < Min:
