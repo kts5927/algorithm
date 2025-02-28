2b77fb5c9a6aec539b5cf8891e0547dfefab43be
kts5927 <154499036+kts5927@users.noreply.github.com>
2025-02-28 20:09:12 +0900

    [Bronze II] Title: 맥주 냉장고, Time: 572 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/3595. 맥주 냉장고/맥주 냉장고.py
@@ -0,0 +1,18 @@
+surface = float('inf')
+n = int(input())
+
+
+ans = [1,1,1]
+Min = float('inf')
+for a in range(1,n+1):
+    if n%a == 0:
+        bc = n//a
+        
+        for b in range(1,bc):
+            if bc%b == 0:
+                c = bc//b
+                if a*b + b*c + c*a < Min:
+                    Min = min(Min,a*b + b*c + c*a)
+                    ans = [a,b,c]
+print(*ans)
+                
