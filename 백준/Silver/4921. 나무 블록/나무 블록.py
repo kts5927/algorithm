0de7f39324c7aa60d83f5341085c52d7e9983330
kts5927 <154499036+kts5927@users.noreply.github.com>
2025-03-08 22:40:19 +0900

    [Silver IV] Title: 나무 블록, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4921. 나무 블록/나무 블록.py
+++ 백준/Silver/4921. 나무 블록/나무 블록.py
@@ -1,28 +1,18 @@
 import sys
 input = sys.stdin.readline
-next = {
-    1: {4, 5},
-    2: {},
-    3: {4, 5},
-    4: {2, 3},
-    5: {8},
-    6: {2, 3},
-    7: {8},
-    8: {6, 7}
-}
-num = 0
+def f(s):
+    d = {
+		'1': {'4','5'},'2': {},'3': {'4','5'},'4': {'2','3'},'5': {'8'},'6': {'2','3'},'7': {'8'},'8': {'6','7'}}
+    if s[0] != '1' or s[-1] != '2':
+        return 'NOT'
+    for i in range(0, len(s) - 1):
+        if s[i+1] not in d[s[i]]:
+            return 'NOT'
+    return 'VALID'
+i = 0
 while True:
-
-    num += 1
-    block = list(input().rstrip())
-    if block == ['0']:
+    s = list(input().rstrip())
+    if s == ['0']:
         break
-    if block[0] != '1' or block[-1] != '2':
-        print(f'{num}.','NOT')
-        continue
-    vn = 'VALID'
-    for i in range(len(block)-1):
-        if int(block[i+1]) not in next[int(block[i])]:
-            vn = 'NOT'
-            break
-    print(f'{num}.',vn)
+    i += 1
+    print(f'{i}.',f(s))
