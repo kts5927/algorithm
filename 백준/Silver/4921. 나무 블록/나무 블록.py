561230c6644a580066ab3d12107d4cc84e3a7149
kts5927 <154499036+kts5927@users.noreply.github.com>
2025-03-08 22:24:36 +0900

    [Silver IV] Title: 나무 블록, Time: 332 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4921. 나무 블록/나무 블록.py
+++ 백준/Silver/4921. 나무 블록/나무 블록.py
@@ -1,26 +1,38 @@
-next = [[], [4,5], [], [4,5], [2,3], [8], [2,3], [8], [6,7]]
+import sys
+
+input = sys.stdin.readline
+
+block_rules = {
+    1: {4, 5},
+    3: {4, 5},
+    4: {2, 3},
+    5: {8},
+    6: {2, 3},
+    7: {8},
+    8: {6, 7}
+}
+
 num = 0
 
 while True:
-    N = int(input())
-    if N == 0:
+    N = input().strip()
+    if N == "0":
         break
-    
 
     num += 1
-    block = list(str(N).strip()) 
-    if block[0] != '1':
+    blocks = list(N)
+
+    if blocks[0] != '1':
         print(f"{num}. NOT")
         continue
 
     valid = True
-    for i in range(len(block) - 1):
-        if int(block[i+1]) not in next[int(block[i])]:
-            print(f"{num}. NOT")
+    for i in range(len(blocks) - 1):
+        if int(blocks[i+1]) not in block_rules.get(int(blocks[i]), set()):
             valid = False
             break
 
-    if valid and block[-1] != '2': 
-        print(f"{num}. NOT")
-    elif valid:
-        print(f"{num}. VALID")
+    if valid and blocks[-1] != '2':
+        valid = False
+
+    print(f"{num}. {'VALID' if valid else 'NOT'}")
