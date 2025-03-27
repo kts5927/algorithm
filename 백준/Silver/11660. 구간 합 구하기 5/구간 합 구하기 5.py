443101c2e30f241463071d5540b93237d54d6d82
kts5927 <154499036+kts5927@users.noreply.github.com>
2025-03-28 04:56:00 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 644 ms, Memory: 73620 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,19 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-N,M = map(int,input().split())
+def sol():  
+    N,M = map(int,input().split())
 
-DP = [[0] * (N+1)]
-for i in range(N):
-    lst = list(map(int,input().split()))
-    DP.append([0] + lst)
-
-
-for i in range(1,N+1):
-    for j in range(1,N+1):
-        DP[i][j] += DP[i-1][j] + DP[i][j-1] - DP[i-1][j-1]
+    DP = [[0] * (N+1)]
+    for i in range(N):
+        lst = list(map(int,input().split()))
+        DP.append([0] + lst)
 
 
-for i in range(M):
-    x1,y1,x2,y2 = map(int,input().split())
-    print(DP[x2][y2] - DP[x1-1][y2] - DP[x2][y1-1] + DP[x1-1][y1-1])
+    for i in range(1,N+1):
+        for j in range(1,N+1):
+            DP[i][j] += DP[i-1][j] + DP[i][j-1] - DP[i-1][j-1]
+
+
+    for i in range(M):
+        x1,y1,x2,y2 = map(int,input().split())
+        print(DP[x2][y2] - DP[x1-1][y2] - DP[x2][y1-1] + DP[x1-1][y1-1])
+sol()
