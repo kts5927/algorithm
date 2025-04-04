8229a15fa4fd9c6607045c14edd1112e272ca507
kts5927 <154499036+kts5927@users.noreply.github.com>
2025-04-05 04:19:33 +0900

    [Silver I] Title: 아이템 배치하기, Time: 456 ms, Memory: 80688 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/21599. 아이템 배치하기/아이템 배치하기.py
+++ 백준/Silver/21599. 아이템 배치하기/아이템 배치하기.py
@@ -1,28 +1,14 @@
-import sys
-input = sys.stdin.read
+T = int(input())
+lst = list(map(int,input().split()))
+lst.sort(reverse=True)
 
-def solve():
-    data = input().split()
-    N = int(data[0])
-    A = list(map(int, data[1:]))
-
-    items = list(enumerate(A))
-    items.sort(key=lambda x: -x[1])
-
-    used = [0] * N  
-    placed = [0] * N  
-
-    for i in range(N):
-        placed[i] = items[i][0]
-
-    strengthened = set()
-    for i in range(N):
-        idx = placed[i]
-        power = A[idx]
-        for j in range(power):
-            pos = (i + j) % N
-            strengthened.add(pos)
-
-    print(len(strengthened))
-
-solve()
+power = 0
+ans = 0
+for i in range(T):
+    power = max(lst[i],power)
+    if power == 0:
+        break
+    ans += 1
+    power -= 1
+    
+print(ans)
