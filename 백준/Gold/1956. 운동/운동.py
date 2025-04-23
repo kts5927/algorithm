78c7d634f9c7b0eb2372aee6a262eda0a786de68
kts5927 <154499036+kts5927@users.noreply.github.com>
2025-04-24 03:44:18 +0900

    [Gold IV] Title: 운동, Time: 1108 ms, Memory: 113848 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+def sol():
+    n,m = map(int,input().split())
+    graph = [[float('inf')] * (n + 1) for _ in range(n + 1)]
+
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            if a == b:
+                graph[a][b] = 0
+
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        graph[a][b] = c
+
+    for k in range(1, n + 1):
+        for a in range(1, n + 1):
+            for b in range(1, n + 1):
+                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+    fw = [[0]*(n+1) for _ in range(n+1)]
+    ans = float('inf')
+    for i in range(1,n+1):
+        for j in range(i,n+1):
+            if i != j:
+                ans = min(ans,graph[i][j] + graph[j][i])
+
+    if ans != float('inf'):
+        print(ans)
+    else:
+        print(-1)
+sol()
